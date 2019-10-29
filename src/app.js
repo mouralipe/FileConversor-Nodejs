5476f079c38cc4319a5b45c053fe7253c772a5a8
mouralipe <felipemoura92@hotmail.com>
2019-10-29 12:26:36 -0300

    heroku config

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 import routes from "./routes";
 
+import cors from "cors";
+
 class App {
   constructor() {
     this.server = express();
@@ -11,6 +13,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
   }
 
